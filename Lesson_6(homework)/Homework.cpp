a7775f7ed44da4ef56cd9b746a8d331d648868d9
villsun <lonukkola@gmail.com>
2024-02-12 19:54:02 +0100

    Add structure Animal

--- Lesson_6(homework)/Homework.cpp
+++ Lesson_6(homework)/Homework.cpp
@@ -36,11 +36,25 @@ struct Boiler
 	double heating_temperature;
 };
 
+struct Animal
+{
+	char name_animal[50];
+	char class_animal[50];
+	char alias_animal[50];
+};
+
+void Fill_structure(Animal& animal, const char* name_animal, const char* class_animal, const char* alias_animal)
+{
+	strcpy(animal.name_animal, name_animal);
+	strcpy(animal.class_animal, class_animal);
+	strcpy(animal.alias_animal, alias_animal);
+}
+
 int main()
 {
 	// Завдання 1
 
-	Washing_Machine washing_machine_1;
+	/*Washing_Machine washing_machine_1;
 
 	strcpy(washing_machine_1.brand, "Samsung");
 	strcpy(washing_machine_1.color, "white");
@@ -58,11 +72,11 @@ int main()
 	cout << "Height = " << washing_machine_1.height << endl;
 	cout << "Power = " << washing_machine_1.power << endl;
 	cout << "Spin speed = " << washing_machine_1.spin_speed << endl;
-	cout << "Heating temperature = " << washing_machine_1.heating_temperature << endl;
+	cout << "Heating temperature = " << washing_machine_1.heating_temperature << endl;*/
 
 	// Завдання 2
 
-	Iron iron_1;
+	/*Iron iron_1;
 
 	strcpy(iron_1.brand, "Apple");
 	strcpy(iron_1.type, "1 plus");
@@ -78,11 +92,11 @@ int main()
 	cout << "Minimum temperature = " << iron_1.min_temperature << endl;
 	cout << "Maximum temperature = " << iron_1.max_temperature << endl;
 	cout << "Steam supply = " << iron_1.steam_supply << endl;
-	cout << "Power = " << iron_1.power << endl;
+	cout << "Power = " << iron_1.power << endl;*/
 
 	// Завдання 3
 
-	Boiler boiler_1;
+	/*Boiler boiler_1;
 
 	strcpy(boiler_1.brand, "HP");
 	strcpy(boiler_1.color, "lime");
@@ -94,9 +108,7 @@ int main()
 	cout << "Color = " << boiler_1.color << endl;
 	cout << "Power = " << boiler_1.power << endl;
 	cout << "Amount = " << boiler_1.amount << endl;
-	cout << "Heating temperature = " << boiler_1.heating_temperature << endl;
-
-
+	cout << "Heating temperature = " << boiler_1.heating_temperature << endl;*/
 
 	return 0;
 }
